2cfd2ee0cc411debbebdc8d9f5e56a26f3b1e7da
mickeyhoang <mickeyhoang1996@gmail.com>
2018-03-03 15:04:50 -0800

    Added function signatures for easy use

--- disasterLibrary.py
+++ disasterLibrary.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 
+# This is the library for inserting new info and accessing info based on searches
+# Check for the function signatures for clear and simple ways to use the functions of this library
+
 # Insertion
 def addShelter(name, zipcode, address, capacity, services, active):
 	client = MongoClient("mongodb://scuhfh:gobroncos@disasterinfo-shard-00-00-vhxix.mongodb.net:27017,disasterinfo-shard-00-01-vhxix.mongodb.net:27017,disasterinfo-shard-00-02-vhxix.mongodb.net:27017/test?ssl=true&replicaSet=DisasterInfo-shard-0&authSource=admin")
@@ -31,14 +34,15 @@ def addProvisions(name, address, zipcode, active):
 	result = provisions.insert_one(provision_data)
 	return ('Posted: {0}'.format(result.inserted_id))
 
-def adduserInfo(name, zipcode):
+def adduserInfo(name, zipcode, phoneNum):
 	client = MongoClient("mongodb://scuhfh:gobroncos@disasterinfo-shard-00-00-vhxix.mongodb.net:27017,disasterinfo-shard-00-01-vhxix.mongodb.net:27017,disasterinfo-shard-00-02-vhxix.mongodb.net:27017/test?ssl=true&replicaSet=DisasterInfo-shard-0&authSource=admin")
 	db = client.DisasterInfo
 	userInfo = db.userInfo
 	
 	user_data = { # Create data packet
 		'Name': name,
-		'ZIP': zipcode
+		'ZIP': zipcode,
+      	'Phone Number': phoneNumb
 	}
 	result = userInfo.insert_one(user_data)
 	return ('Post ID: {0}'.format(result.inserted_id))
@@ -73,10 +77,10 @@ def getProvisions(zipcode):
 	provisions_posts = provisions.find({'ZIP' : zipcode})
 	return provisions_posts
 
-def getProvisions(zipcode):
+def getUserInfo(phoneNum):
 	client = MongoClient("mongodb://scuhfh:gobroncos@disasterinfo-shard-00-00-vhxix.mongodb.net:27017,disasterinfo-shard-00-01-vhxix.mongodb.net:27017,disasterinfo-shard-00-02-vhxix.mongodb.net:27017/test?ssl=true&replicaSet=DisasterInfo-shard-0&authSource=admin")
 	db = client.DisasterInfo
-	provisions = db.provisions # Collection name inside database
+	userInfo = db.userInfo # Collection name inside database
 
-	provisions_posts = provisions.find({'ZIP' : zipcode})
-	return provisions_posts
+	userInfo_post = userInfo.find_one({'Phone Number' : phoneNum})
+	return userInfo_post
